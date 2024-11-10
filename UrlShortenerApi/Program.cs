02405e9d69f5c319877f0a22208c21bfebc7bc93
Nomonjon <nmatlubov@gmail.com>
2024-11-10 19:24:44 +0500

    New proj

---
+++ UrlShortenerApi/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortenerApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Регистрируем UrlShortener как Singleton
+builder.Services.AddSingleton(new UrlShortener(Environment.GetEnvironmentVariable("HOSTNAME") ?? "https://url.ilmhub.uz"));
+
+// Добавляем поддержку контроллеров
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Включаем маршрутизацию для контроллеров
+app.MapControllers();
+
+app.Run();
---
+++ urlsh/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using url.shortener.cli.urlsh;
+
+string host = Environment.GetEnvironmentVariable("HOSTNAME") ?? "https://url.ilmhub.uz";
+if (args.Length > 0)
+{
+    host = args[0];
+}
+
+UrlShortener urlShortener = new UrlShortener(host);
+
+Console.WriteLine("Введите длинный URL для сокращения:");
+string longUrl = Console.ReadLine() ?? throw new ArgumentException("Invalid URL");
+string shortUrl = urlShortener.ShortenUrl(longUrl);
+Console.WriteLine($"Сокращенный URL: {shortUrl}");
+
+Console.WriteLine("Введите короткий код для получения оригинального URL:");
+string inputCode = Console.ReadLine() ?? throw new ArgumentException("Invalid URL");
+string originalUrl = urlShortener.GetOriginalUrl(inputCode.Replace(host + "/", ""));
+Console.WriteLine($"Оригинальный URL: {originalUrl}");
+
